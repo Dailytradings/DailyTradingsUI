659e38a60bd229f5f43dbe4e19f4db015da3d07e
kadirkalkan <kadir.kalkan3475@gmail.com>
2021-02-25 08:49:22 -0500

    final merge process

--- src/app/layouts/widgets/locked-panel/locked-panel.component.ts
+++ src/app/layouts/widgets/locked-panel/locked-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectorRef, Component, Inject, Input, OnInit, PLATFORM_ID } from '@angular/core';
 import { AuthService } from 'app/shared/auth/auth.service';
 
 @Component({
@@ -12,10 +13,13 @@ export class LockedPanelComponent implements OnInit {
   @Input() allowedToSee;
   loggedUser;
 
-  constructor(private authService: AuthService) { }
+  constructor(@Inject(PLATFORM_ID) private _platformId: Object, private authService: AuthService, private cdRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    this.loggedUser = this.authService.isAuthenticated();
+    if (isPlatformBrowser(this._platformId)) {
+      this.loggedUser = this.authService.isAuthenticated();
+      this.cdRef.detectChanges();
+    }
   }
 
 }
